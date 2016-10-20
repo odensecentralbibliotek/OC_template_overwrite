c8c44cc43768c1dca8c3be5335e5c530cdcd95ea
Tobias F. Petersen <tobsf@hotmail.com>
2016-10-20 11:04:48 +0200

    ting_reference_cover_preview v0.2 Code strukture update now live retrival of preview.

--- js/oc_ting_reference_cover_preview.js
+++ js/oc_ting_reference_cover_preview.js
@@ -6,44 +6,29 @@
 jQuery(document).ready(function ($) {
     if(!Drupal.settings.oc_template_overwrites.oc_material_img_preview_enabled)
     {
-        return
+        return;
     }
     var ting_objects = [];
     var keyd_inputs = {};
-    //ting_objects.push({local_id:'22629344' , image_style: 'ding_medium' , owner_id: '746100'});
-
+    /*
+     * Bind preview creation for newly entered fields.
+     */
+    var Inputs = $('#edit-field-ding-news-materials').on('blur','input',update_preview);
+    /*
+     * Create Previews for non-empty fields
+     */
     var Inputs = $('#edit-field-ding-news-materials input[value!=""]:not(".ajax-processed")');
+
+    var preloader = get_preloader_elem();
     $.each(Inputs, function (index,elem) {
-        
-        var localId = elem.value.split(':');
-        if(localId.length == 1)
-        {
-            localId = localId[0];
-        }
-        else
-        {
-            localId = localId[1];
-        }
+        var localId = parse_local_id(elem.value);
+ 
         ting_objects.push({local_id: localId, image_style: 'ding_medium', owner_id: '746100'});
         keyd_inputs[localId] = elem; //so their easier to pair with their images.
+        $(elem).before(preloader);
     });
     
-    if (ting_objects.length != 0)
-    {
-        $.ajax({
-            type: "POST",
-            url: "/ting/covers",
-            content: "application/json, charset=utf8",
-            data: {coverData: ting_objects}
-        })
-                .done(function (Data) {
-                    $.each(Data,function(index,dataobj){
-                        var elem = keyd_inputs[dataobj['local_id']];
-                        var img = $('<img style="vertical-align:middle;height:27px;margin-right:5px;cursor:pointer;" />').attr('src',dataobj['url']).click(show_large_image);
-                        $(elem).before(img);
-                    });
-                });
-    }
+    update_cover_previews(keyd_inputs,ting_objects);
 
     function show_large_image(imageElem)
     {
@@ -60,6 +45,82 @@ jQuery(document).ready(function ($) {
               }
             }
           });
+    }
+    function update_preview(elem)
+    {
+        debugger;
+        var Target = $(elem.currentTarget);
+        var localId = parse_local_id(Target.val());
+        /*
+         * Exit if empty
+         */
+        if(localId == "")
+        {
+            return;
+        }
+        
+        var old_preview_elem = Target.parent().find('.cover_preview');
+        var old_local_id = null;
+        if(old_preview_elem.length != 0)
+        {
+           /*
+            * Get the old cover id
+            */
+           old_local_id = parse_local_id(old_preview_elem.attr('id'))
+        }
+        /*
+        * Only update the cover if it is different id.
+        */
+        if(localId != old_local_id)
+        {
+            var keyd_inputs = {};
+            var ting_objects = [];
+            ting_objects.push({local_id: localId, image_style: 'ding_medium', owner_id: '746100'});
+            keyd_inputs[localId] = elem.currentTarget; //so their easier to pair with their images.
+            
+            old_preview_elem.remove();
+            Target.before(get_preloader_elem());
+
+            update_cover_previews(keyd_inputs,ting_objects);
+        }
+    }
+    function update_cover_previews(keyd_inputs,ting_objects)
+    {
+        if (ting_objects.length != 0)
+        {
+            $.ajax({
+                type: "POST",
+                url: "/ting/covers",
+                content: "application/json, charset=utf8",
+                data: {coverData: ting_objects}
+            })
+                    .done(function (Data) {
+                        $.each(Data,function(index,dataobj){
+                            var elem = keyd_inputs[dataobj['local_id']];
+                            var img = $('<img id="'+dataobj['local_id']+'" class="cover_preview" style="vertical-align:middle;height:27px;margin-right:5px;cursor:pointer;" />').attr('src',dataobj['url']).click(show_large_image);
+                            $(elem).parent().find('#oc_material_preview_preloader').remove();
+                            $(elem).before(img);
+                        });
+                    });
+        }
+    }
+    function parse_local_id(id)
+    {
+        var localId = id.split(':');
+        if(localId.length == 1)
+        {
+            localId = localId[0];
+        }
+        else
+        {
+            localId = localId[1];
+        }
+        return localId;
+    }
+    function get_preloader_elem()
+    {
+        var spinnerUrl = Drupal.settings.basePath + "files/362.GIF";
+        return $('<img style="vertical-align:middle;height:27px;margin-right:5px;" id="oc_material_preview_preloader" src="'+spinnerUrl+'" />');
     }
 });
 
