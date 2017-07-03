6e0f974ccfd4f4d393e06d53e575c382da5c8e0d
Tobias F. Petersen <tobsf@hotmail.com>
2017-07-03 10:00:08 +0200

    Enable material descriptions for ding_page

---
+++ js/material_descriptions_page.js
@@ -0,0 +1,105 @@
+(function ($) {
+    /**
+     * Load remote content after the main page loaded.
+     */
+    Drupal.behaviors.material_description_page = {
+        attach: function (context, settings) {
+            debugger;
+            /*
+             * This is needed to make the jquery dialog not disable the ckeditor
+             * text fields.
+             * see: http://stackoverflow.com/questions/15348209/jquery-modal-dialog-and-ckeditor-input-dialog
+             */
+            $.widget("ui.dialog", $.ui.dialog, {
+                _allowInteraction: function (event) {
+                    return 1; // Hack to allow CKE dialog interactions in all browsers. 
+                    //return !!$(event.target).closest(".cke_dialog").length || this._super(event);
+                }
+            });
+            /*
+             * add a new material description , if the number of materials
+             * are larger then the number of descriptions , else dont.
+             */
+            var elem = $('.field-name-field-ding-page-materials').find('input[type="submit"]');
+            elem.mousedown(function () {
+                var material_children = $('.field-name-field-ding-page-materials').find('.form-item').length;
+                var description_children = $('.field-name-field-ding-page-material-descrip').find('.form-item').length;
+                if (material_children >= description_children)
+                {
+                    setTimeout(function(){
+                        $('.field-name-field-ding-page-material-descrip').find('input[type="submit"]').mousedown();
+                    },650);
+                }
+            });
+            /*
+             *  Add wysiwyg abillity for material descriptions.
+             */
+            
+            var inputs = $('#edit-field-ding-page-material-descrip table input:not(.field-add-more-submit)');
+            inputs.unbind('click');
+            inputs.click(function (e) {
+                if(CKEDITOR.instances.mat_descrip_editor != undefined)
+                {
+                   CKEDITOR.instances.mat_descrip_editor.destroy(); // ensure death.
+                }
+                //Define global so we can access the value later in the dialog.
+                current_material_description_input = $(e.currentTarget);
+
+                //build modal.
+                var DivDialog = $(document.createElement('div'));
+                DivDialog.attr('id','material_descrip_dialog')
+                var old_text = current_material_description_input.val();
+                DivDialog.html('<textarea name="editor123" id="mat_descrip_editor" rows="10" cols="80">' + old_text + '</textarea>');
+                DivDialog.dialog({
+                    title: 'Rediger materiale beskrivelse',
+                    resizable: false,
+                    height: 500,
+                    width: 500,
+                    modal: true,
+                    buttons: {
+                        "Gem": function () {
+                            var new_text = CKEDITOR.instances.mat_descrip_editor.getData();
+                            current_material_description_input.val(new_text);
+                            CKEDITOR.instances.mat_descrip_editor.destroy();
+                            $(this).dialog("close");
+                            $(this).dialog("destroy");
+
+                        },
+                        "Annuler": function () {
+                            CKEDITOR.instances.mat_descrip_editor.destroy();
+                            $(this).dialog("close");
+                            $(this).dialog("destroy");
+                        }
+                    },
+                    open: function ()
+                    {
+                        /*
+                         * Handle dialog close.
+                         * this does not work work well out of the box.
+                         */
+                         $('body').on('click', ".ui-dialog-titlebar-close" ,function() { 
+                             if(CKEDITOR.instances.mat_descrip_editor != undefined)
+                             {
+                                 CKEDITOR.instances.mat_descrip_editor.destroy();
+                                  $('#material_descrip_dialog').dialog("close");
+                                  $('#material_descrip_dialog').dialog("destroy");
+                             }
+                             
+                         });
+                        /*
+                         * Spawn the ckeditor on dialog open.
+                         */
+                        CKEDITOR.replace('mat_descrip_editor', {
+                            // Define the toolbar groups as it is a more accessible solution.
+                            toolbarGroups: [
+                                {"name": "links", "groups": ["links"]},
+                            ],
+                            // Remove the redundant buttons from toolbar groups defined above.
+                            removeButtons: 'Underline,Strike,Subscript,Superscript,Anchor,Styles,Specialchar,paragraph,basicstyles,document,insert,styles,about',
+                        });
+                    },
+                });
+            });
+        }
+    };
+})(jQuery);
---
+++ js/oc_ting_reference_cover_preview_page.js
@@ -0,0 +1,153 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+jQuery(document).ready(function ($) {
+    Drupal.behaviors.oc_ting_reference_cover_preview = {
+        attach: function (context, settings) {
+            debugger;
+            try {
+                var test = context.first();
+                var idstr = test.attr('id');
+                if(idstr != undefined && idstr.indexOf('field-ding-page-materials-add-more-wrapper') !== -1)
+                {
+                    var Inputs = $('.field-name-field-ding-page-materials input[value!=""]:not(".ajax-processed")');
+                    Inputs.trigger('blur');
+                }
+            } catch (e) {
+                
+            }
+
+            
+        }
+    }
+    init_cover_preview();
+    function init_cover_preview()
+    {
+        if(Drupal.settings.oc_template_overwrites.oc_material_img_preview_enabled == 0)
+        {
+            return;
+        }
+        var ting_objects = [];
+        var keyd_inputs = {};
+       /*
+        * Bind preview creation for newly entered fields.
+        */
+       var Inputs = $('.field-name-field-ding-page-materials').on('blur','input',update_preview);
+        /*
+         * Create Previews for non-empty fields
+         */
+        var Inputs = $('.field-name-field-ding-page-materials input[value!=""]:not(".ajax-processed")');
+
+        var preloader = get_preloader_elem();
+        $.each(Inputs, function (index,elem) {
+            var localId = parse_local_id(elem.value);
+
+            ting_objects.push({local_id: localId, image_style: 'ding_medium', owner_id: '746100'});
+            keyd_inputs[localId] = elem; //so their easier to pair with their images.
+            $(elem).before(preloader);
+        });
+
+        update_cover_previews(keyd_inputs,ting_objects);
+    }
+    function show_large_image(imageElem)
+    {
+        var the_dialog = $('<div id="dialog" title=""><img style="" id="oc_image_preview" src="'+$(imageElem.currentTarget).attr('src')+'" /></div>').dialog({
+            title: Drupal.t("OC Materiale Viewer"),
+            resizable: false,
+            height: "auto",
+            width: 400,
+            modal: true,
+            buttons: {
+              ok: function() {
+                $( this ).dialog( "close" );
+                $( this ).dialog( "destroy" );
+              }
+            }
+          });
+    }
+    function update_preview(elem)
+    {
+
+        var Target = $(elem.currentTarget);
+        var localId = parse_local_id(Target.val());
+        if(localId == "")
+        {
+            return;
+        }
+        var img_exists = Target.parent().find(('#'+localId));
+        /*
+         * Exit if empty
+         */
+        if(localId == "" || img_exists.length != 0)
+        {
+            return;
+        }
+        
+        var old_preview_elem = Target.parent().find('.cover_preview');
+        var old_local_id = null;
+        if(old_preview_elem.length != 0)
+        {
+           /*
+            * Get the old cover id
+            */
+           old_local_id = parse_local_id(old_preview_elem.attr('id'))
+        }
+        /*
+        * Only update the cover if it is different id.
+        */
+        if(localId != old_local_id)
+        {
+            var keyd_inputs = {};
+            var ting_objects = [];
+            ting_objects.push({local_id: localId, image_style: 'ding_medium', owner_id: '746100'});
+            keyd_inputs[localId] = elem.currentTarget; //so their easier to pair with their images.
+            
+            old_preview_elem.remove();
+            Target.before(get_preloader_elem());
+
+            update_cover_previews(keyd_inputs,ting_objects);
+        }
+    }
+    function update_cover_previews(keyd_inputs,ting_objects)
+    {
+        if (ting_objects.length != 0)
+        {
+            $.ajax({
+                type: "POST",
+                url: "/ting/covers",
+                content: "application/json, charset=utf8",
+                data: {coverData: ting_objects}
+            })
+                    .done(function (Data) {
+                        $.each(Data,function(index,dataobj){
+                            var elem = keyd_inputs[dataobj['local_id']];
+                            var img = $('<img id="'+dataobj['local_id']+'" class="cover_preview" style="vertical-align:middle;height:27px;margin-right:5px;cursor:pointer;" />').attr('src',dataobj['url']).click(show_large_image);
+                            $(elem).parent().find('#oc_material_preview_preloader').remove();
+                            $(elem).before(img);
+                        });
+                        $('body').find('#oc_material_preview_preloader').remove();
+                    });
+        }
+    }
+    function parse_local_id(id)
+    {
+        var localId = id.split(':');
+        if(localId.length == 1)
+        {
+            localId = localId[0];
+        }
+        else
+        {
+            localId = localId[1];
+        }
+        return localId;
+    }
+    function get_preloader_elem()
+    {
+        var spinnerUrl = Drupal.settings.basePath + "files/362.GIF";
+        return $('<img style="vertical-align:middle;height:27px;margin-right:5px;" id="oc_material_preview_preloader" src="'+spinnerUrl+'" />');
+    }
+});
+
